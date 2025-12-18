6e0b3955f4671b50e4df9c4738195056ef7ac57d
mwanga45 <issamwanga02@gmail.com>
2025-12-18 11:23:51 +0300

    more changes

--- record.dart
+++ record.dart
@@ -1,3 +1,15 @@
-void main(){
-  
+void main() {
+  var lakes = ('tanganika', 'victoria', kenya: 'victoria');
+  print(lakes);
+  print(lakes.$1);
+  var result = Music(100, 'somtheing');
+  print(result.$1);
+}
+
+(int, String) Music(int a, String b) {
+  if (a > 5) {
+    return (5, 'weekend');
+  } else {
+    return (a, 'not weekend');
+  }
 }
