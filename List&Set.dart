d49d2cb0bde1cea2be2393a60d51fb2adcf47e40
mwanga45 <issamwanga02@gmail.com>
2025-12-19 10:52:10 +0300

    more changes

--- List&Set.dart
+++ List&Set.dart
@@ -1,3 +1,10 @@
-void main(){
-  
+void main() {
+  List<int> score = [1, 20, 50, 24, 46];
+  score.add(55);
+  print(score);
+  score.remove(50);
+  score.removeAt(2);
+  print(score);
+  score.shuffle();
+  print(score);
 }
