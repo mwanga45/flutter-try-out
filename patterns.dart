760dbcca9388f1a200fafa6195e4491844ae7ec7
mwanga45 <issamwanga02@gmail.com>
2025-12-19 08:46:01 +0300

    more changes

---
+++ patterns.dart
@@ -0,0 +1,14 @@
+void main() {
+  // without patterns
+  // var identification = ("issa", 2002, "kilimanjaro");
+  // print(identification.$1);
+  // print(identification.$2);
+  // print(identification.$3);
+
+  // with patterns 
+
+ var (name, year, place) = ("issa", 2002, "kilimanjaro");
+  print(name);
+  print(year);
+  print(place);
+}
